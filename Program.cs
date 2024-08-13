314a47b5e87dfd2921ab1b985f0446ca506e328b
Felipe Rondello <ferondellodev@gmail.com>
2024-08-13 14:30:56 -0300

    classes montadas

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
